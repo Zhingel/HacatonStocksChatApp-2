56c088d5e06c4c3276349b3e69eb457535207f95
Stas <staszhingel@icloud.com>
2021-11-26 23:21:55 +0300

    profileView

--- HacatonStocksChatApp/MainTabBarController.swift
+++ HacatonStocksChatApp/MainTabBarController.swift
@@ -23,7 +23,7 @@ class MainTabBarController: UITabBarController {
     fileprivate func setupViewControllers() {
         view.backgroundColor = .white
         let layout = UICollectionViewFlowLayout()
-        let viewController = FeedViewController()
+        let viewController = ProfileViewController()
         let navViewController = UINavigationController(rootViewController: viewController)
         navViewController.tabBarItem.image = UIImage(systemName: "person.circle")
         navViewController.tabBarItem.selectedImage = UIImage(systemName: "person.circle")
---
+++ HacatonStocksChatApp/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  HacatonStocksChatApp
+//
+//  Created by Стас Жингель on 26.11.2021.
+//
+
+import Foundation
+import UIKit
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
